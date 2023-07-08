8fac043291e759922895fcd05182c706f3e4eb7b
Erkin IT <erkin.it@gmail.com>
2023-07-08 20:38:04 +0600

    Add home work 25

---
+++ HW25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+string InputTxt (string TxtSrting) // Модуль приглашения для ввода данных в виде текста с клавиатуры
+{
+    Console.Write (TxtSrting);
+    return Console.ReadLine();
+}
+
+int A = int.Parse(InputTxt("Введите число которое необходимо возвести в степень: "));
+int B = int.Parse(InputTxt("Введите степень числа: "));
+int result = 1;
+
+if (B > 0)
+{
+    for (int i = 0; i < B;i++ )
+    {
+        result *= A;
+    }
+    Console.WriteLine ($"Число {A} в степени {B} равен {result}");
+}
+else
+{
+    Console.WriteLine ("Степень должна быть больше нуля");
+}
