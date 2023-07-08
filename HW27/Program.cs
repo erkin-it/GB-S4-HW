8f2ccf9f44ffc44bd77b9a60a86505181d24d477
Erkin IT <erkin.it@gmail.com>
2023-07-08 21:02:47 +0600

    Add home work 27

---
+++ HW27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+string InputTxt (string TxtSrting) // Модуль приглашения для ввода данных в виде текста с клавиатуры
+{
+    Console.Write (TxtSrting);
+    return Console.ReadLine();
+}
+
+int SumNumbers (int InputNumber)
+{
+    int IN = Math.Abs(InputNumber);
+    int R = 0;
+    int T = 0;
+    do 
+        {
+            T=IN%10;
+            R=R+T;
+            IN=IN/10;
+        }
+    while (IN > 0);
+    return R;
+}
+
+//------------------------------------------------------------------------------------------------
+
+int Number = int.Parse(InputTxt("Введите число: "));
+
+int Result = SumNumbers (Number);
+
+Console.WriteLine($"Сумма цифр в числе равна {Result}");
